ed2a8e68b947db701e37525d304a85349bb507a7
sourabh <sourabhraghav30@gmail.com>
2020-12-06 23:46:36 +0530

    Initialized App using create-react-app and firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAJi5HTDvdTdRLaJACgxRt47wp0nDi_kb8",
+  authDomain: "clone-b7477.firebaseapp.com",
+  projectId: "clone-b7477",
+  storageBucket: "clone-b7477.appspot.com",
+  messagingSenderId: "476811957103",
+  appId: "1:476811957103:web:dfe6ef6159a3d972cdce9a",
+};
